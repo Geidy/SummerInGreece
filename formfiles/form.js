e62ea138856bbbe76551fd843e3726b4d950ab54
Geidy D Molina <44411149+Geidy@users.noreply.github.com>
2019-08-20 14:51:03 -0400

    Added a React form to an html file

---
+++ formfiles/form.js
@@ -0,0 +1,95 @@
+// import React from 'react';
+// import '../css/FormStyle.css';
+
+'use strict';
+
+
+const e = React.createElement;
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value});
+        console.log(event.target)    //always console.log so you can see the changes
+    }
+
+    clearForm() {
+        console.log(this.state)
+        this.setState({ name: '', email: '', college: '', info: '' })
+
+    }
+
+    handleSubmit(event) {
+        alert('Thank you for your request : ' + this.state.name + '; We will send you a response asap at your prefered email address: ' 
+        + this.state.email + '; And  we will request more info from your current college: ' 
+        + this.state.college );
+        event.preventDefault();
+        this.clearForm();
+    }
+
+   
+
+
+    render() {
+        //(this.state.form)
+        return (
+            <div className="Modal">
+
+                <form className="form" onSubmit={this.handleSubmit}>
+                    <div className="container">
+                        <label className="labels">Full Name </label>
+                        <input type="input" id="name" className="input" value={this.state.name} onChange={this.handleChange} placeholder="Emily Smith" />
+
+                    </div>
+                    <div className="container">
+                        <label className="labels">Prefered Email </label>
+                        <input className="input" id="email" value={this.state.email} onChange={this.handleChange} placeholder="Emily@gmail.com" />
+
+                    </div>
+                    <div className="container">
+                        <label className="labels">College </label>
+                        <input className="input" id="college" value={this.state.college} onChange={this.handleChange} placeholder="Lehman College" />
+
+                    </div>
+                    <div>
+                        <label className="labels">
+                            Info Request:
+                    </label>
+                        <textarea className="textarea" id="info" value={this.state.info} onChange={this.handleChange} placeholder="Info: Study Abroad in Greece" />
+
+                    </div>
+
+
+                    <button className="btn" type="submit" value="Submit">Submit</button>
+                </form>
+
+
+
+
+            </div>
+
+
+
+            
+        );
+    }
+}
+
+
+//export default Form;
+const domContainer = document.querySelector('#form_container');
+ReactDOM.render(e(Form), domContainer);
+
+/*const domContainer = document.querySelector('#form_button_container');
+ReactDOM.render(e(FormButton), domContainer); */
+
+//export default FormButton;
+
